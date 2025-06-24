62ff69713de102bac9bdc26008738120106583fa
30204707 <mariaangelica.valdes@ucalgary.ca>
2025-06-23 20:49:42 -0600

    added connect_email and extract_urls_from_email funcs

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import imaplib
+import email
+from email.header import decode_header
+import re
+
+def connect_email(username, password):
+    imap = imaplib.IMAP4_SSL("imap.gmail.com")
+    imap.login(username, password)
+    imap.select("inbox")
+    return imap
+
+def extract_urls_from_email(email_message):
+    urls = []
+    for part in email_message.walk():
+        if part.get_content_type() == "text/plain":
+            body = part.get_payload(decode=True).decode(errors='ignore')
+            urls.extend(re.findall(r'(https?://\S+)', body))
+    return urls
